aecaa91554ad6eb112c7aced9822060d02595609
rodrigospicker1 <r.spicker13@gmail.com>
2023-02-15 09:28:02 -0300

    question_form

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
-app.set('view engine', 'ejs')
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get("/",(req, res) => {
     res.render("index");
+});
+
+app.get("/pergunta",(req, res) => {
+    res.render("pergunta");
+});
+
+app.get("/salvar_pergunta",(req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido");
 });
 
 app.listen(8080, () => {console.log("App rodando!");});
